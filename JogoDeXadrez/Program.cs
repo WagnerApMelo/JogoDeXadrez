05b4e4517325566e6d7cf1e29a1024066bf3aeb6
WagnerApMelo <wagnermelo36@yahoo.com.br>
2023-02-05 11:41:21 -0300

    Melhorando o tabuleiro

--- JogoDeXadrez/Program.cs
+++ JogoDeXadrez/Program.cs
@@ -3,10 +3,23 @@ using JogoDeXadrez.xadrez;
 using tabuleiro;
 
 
+Tabuleiro tab = new Tabuleiro(8,8);
 
-PosicaoXadrez pos = new PosicaoXadrez('d', 8);
+try
+{
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+    tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 0));
+    tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+    tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(3, 5));
+    Tela.imprimirTabuleiro(tab);
+}
+catch(TabuleiroException e)
+{
+    Console.WriteLine(e.Message);
 
-Console.WriteLine(pos);
-Console.WriteLine(pos.toPosicao());
+}
+
+
+
 Console.ReadLine(); 
 
